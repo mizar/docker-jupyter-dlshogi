fc9e6c4a7b19b660082274894c7f7e8aea5437ef
Mizar <mizar.jp@gmail.com>
2022-01-09 22:24:37 +0900

    `--create-dirs`

--- x02_install_shogigui.bat
+++ x02_install_shogigui.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -command "$file=\"$(((Invoke-WebRequest http://shogigui.siganus.com/download.html -UseBasicParsing).Links.href^|ForEach-Object{$_ -split '/'}^|Where-Object{$_ -like 'ShogiGUIv*.exe'})[0])\";& curl.exe -RLo \".dl/$file\" \"http://shogigui.siganus.com/shogigui/$file\";& \".dl/$file\" /silent"
+powershell -command "$file=\"$(((Invoke-WebRequest http://shogigui.siganus.com/download.html -UseBasicParsing).Links.href^|ForEach-Object{$_ -split '/'}^|Where-Object{$_ -like 'ShogiGUIv*.exe'})[0])\";& curl.exe --create-dirs -RLo \".dl/$file\" \"http://shogigui.siganus.com/shogigui/$file\";& \".dl/$file\" /silent"
