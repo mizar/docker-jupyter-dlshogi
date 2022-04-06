de7ec918057c7fe23cc87c66be59691083e4894b
Mizar <mizar.jp@gmail.com>
2022-04-06 19:30:08 +0900

    fix docker desktop url thanks @lvisdd

--- x05_install_docker_desktop.bat
+++ x05_install_docker_desktop.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-curl --create-dirs -RLo .dl\DockerDesktopInstaller.exe "https://desktop.docker.com/win/stable/amd64/Docker Desktop Installer.exe"
+curl --create-dirs -RLo .dl\DockerDesktopInstaller.exe "https://desktop.docker.com/win/main/amd64/Docker Desktop Installer.exe"
 .dl\DockerDesktopInstaller.exe install --quiet
 echo;
 echo Docker Desktop for Windows is installed. LOGOFF after 5 seconds to complete the installation.
