8092a7bd52dd35835742e822b110d88f0fa2493a
Mizar <mizar.jp@gmail.com>
2022-05-18 19:09:02 +0900

    CUDA 11.7.0 Display.Driver

--- x03_cuda_displaydriver.cmd
+++ x03_cuda_displaydriver.cmd
@@ -1,9 +1,9 @@
 @echo off
 cd %~dp0
-curl --create-dirs -RLo .dl\cuda_11.6.0_windows_network.exe https://developer.download.nvidia.com/compute/cuda/11.6.0/network_installers/cuda_11.6.0_windows_network.exe
+curl --create-dirs -RLo .dl\cuda_11.7.0_windows_network.exe https://developer.download.nvidia.com/compute/cuda/11.7.0/network_installers/cuda_11.7.0_windows_network.exe
 echo;
 echo Installing the CUDA dispaly driver...
-.dl\cuda_11.6.0_windows_network.exe -s Display.Driver
+.dl\cuda_11.7.0_windows_network.exe -s Display.Driver
 echo;
 echo Completed installation of CUDA display driver.
 powershell sleep 5
